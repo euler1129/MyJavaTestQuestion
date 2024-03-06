2e7f36b7461df03478ebb01047f842f73a1b26fa
euler1129 <896498334@qq.com>
2024-03-06 15:24:55 +0800

    死锁测试

---
+++ src/main/java/com/test/question/multi_thread/DeadLockTest.java
@@ -0,0 +1,36 @@
+package com.test.question.multi_thread;
+
+public class DeadLockTest {
+    private static Object lock1 = new Object();
+    private static Object lock2 = new Object();
+    public static void main(String[] args) {
+        new Thread(() -> {
+            synchronized (lock1) {
+                System.out.println("thread1 get lock1");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (lock2) {
+                    System.out.println("thread1 get lock2");
+                }
+                System.out.println("thread1 end");
+            }
+        }).start();
+        new Thread(() -> {
+            synchronized (lock2) {
+                System.out.println("thread2 get lock2");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (lock1) {
+                    System.out.println("thread2 get lock1");
+                }
+                System.out.println("thread2 end");
+            }
+        }).start();
+    }
+}
